622260b35fc4f27bcc94f1457bced00ab45134ce
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-07 18:44:47 +0200

    feat: add Automapper Profile

---
+++ Services/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using DataAccess.Models;
+using Services.DTOs;
+
+namespace Services
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerSearchResultDto>();
+
+            CreateMap<Customer, CustomerDetailsDto>()
+                .ForMember(dest => dest.Accounts, opt => opt.MapFrom(
+                    src => src.Dispositions.Select(d => d.Account)))
+                .ForMember(dest => dest.TotalBalance, opt => opt.MapFrom(
+                    src => src.Dispositions.Sum(d => d.Account.Balance)))
+                .ReverseMap();
+
+            CreateMap<Account, AccountDto>().ReverseMap();
+
+            CreateMap<Transaction, TransactionDto>()
+                .ForMember(dest => dest.ToAccount, opt => opt.MapFrom(src => src.Account))
+                .ForMember(dest => dest.FromAccountId, opt => opt.MapFrom(src => src.AccountId))
+                .ReverseMap();
+        }
+    }
+}
