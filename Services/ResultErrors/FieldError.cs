7fc3cf2831472dad6dfa602edad09a2a90b63a92
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-07 18:31:01 +0200

    feat: add FieldError for better Result errors

---
+++ Services/ResultErrors/FieldError.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+
+namespace Services.ResultErrors
+{
+    public class FieldError : Error
+    {
+        public string Field { get; }
+
+        public FieldError(string field, string message)
+            : base(message)
+        {
+            Field = field;
+        }
+    }
+}
