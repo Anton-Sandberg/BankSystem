bd6a75bf2369ad89f9e542afbfbfd93703121bd4
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-11 15:47:58 +0200

    feat: add CountryBalanceViewModel

---
+++ BankSystem/ViewModels/CountryBalanceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BankSystem.ViewModels
+{
+    public class CountryBalanceViewModel
+    {
+        public string CountryCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public decimal TotalBalance { get; set; }
+    }
+}
