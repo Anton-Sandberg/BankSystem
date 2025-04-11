22dfe5e2554076fd99aaa12c239c8f3a5d70dc37
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-11 15:54:09 +0200

    feat: update Index.cshtml with landing page info

--- BankSystem/Pages/Index.cshtml.cs
+++ BankSystem/Pages/Index.cshtml.cs
@@ -1,20 +1,56 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using BankSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Services;
 
 namespace BankSystem.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IMapper _mapper;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private ICountryBalanceService _countryBalanceService;
+        private ICustomerService _customerService;
+        private IAccountService _accountService;
+
+        public List<CountryBalanceViewModel> CountryBalances { get; set; } = new();
+
+        public int CustomerCount { get; set; }
+        public int AccountCount { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, ICountryBalanceService countryBalanceService, IMapper mapper, ICustomerService customerService, IAccountService accountService)
         {
             _logger = logger;
+            _countryBalanceService = countryBalanceService;
+            _mapper = mapper;
+            _customerService = customerService;
+            _accountService = accountService;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
 
+            try
+            {
+                var countryBalances = await _countryBalanceService.GetBalancePerCountryAsync();
+                CountryBalances = _mapper.Map<List<CountryBalanceViewModel>>(countryBalances);
+
+                CustomerCount = await _customerService.GetCustomerCountAsync();
+                AccountCount = await _accountService.GetAccountCountAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to load dashboard data");
+
+                TempData["ErrorMessage"] = "Unable to load data. Please try again later.";
+
+                CountryBalances = new List<CountryBalanceViewModel>();
+                CustomerCount = 0;
+                AccountCount = 0;
+            }
         }
     }
 }
