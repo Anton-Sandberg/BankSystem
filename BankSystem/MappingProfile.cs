eac216375b42adfac73bf74a6c61e43460a38651
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-11 15:52:41 +0200

    feat: add MappingProfile

---
+++ BankSystem/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BankSystem.ViewModels;
+using Services.DTOs;
+
+namespace BankSystem
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CountryBalanceDto, CountryBalanceViewModel>()
+                    .ForMember(dest => dest.CountryCode, opt => opt.MapFrom(src => CountryFlagHelper.GetCountryCode(src.Country)))
+                    .ReverseMap();
+        }
+    }
+}
