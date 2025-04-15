dcf539a435fb941a5b4d5fdacc0fb65fdd0624c1
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-15 19:41:00 +0200

    feat: add CustomerSearch and update _Layout with link to it

---
+++ BankSystem/Pages/Customers/CustomerSearch.cshtml.cs
@@ -0,0 +1,62 @@
+using System.ComponentModel;
+using System.Threading.Tasks;
+using AutoMapper;
+using BankSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankSystem.Pages.Customers
+{
+    public class CustomerSearchModel : PageModel
+    {
+        private ICustomerService _customerService;
+        private IMapper _mapper;
+        public List<CustomerSearchViewModel> Customers { get; set; } = new();
+
+        public int PageSize { get; set; } = 50;
+
+
+        [BindProperty(SupportsGet = true)]
+        public string SortOrder { get; set; } = string.Empty;
+        [BindProperty(SupportsGet = true)]
+        public string SortColumns { get; set; } = string.Empty;
+
+
+        [DisplayName("First Name")]
+        [BindProperty(SupportsGet = true)]
+        public string GivenName { get; set; } = string.Empty;
+
+        [DisplayName("Last Name")]
+        [BindProperty(SupportsGet = true)]
+        public string SurName { get; set; } = string.Empty;
+
+        [DisplayName("City")]
+        [BindProperty(SupportsGet = true)]
+        public string City { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; }
+
+
+        public CustomerSearchModel(ICustomerService customerService, IMapper mapper)
+        {
+            _customerService = customerService;
+            _mapper = mapper;
+        }
+
+        public async Task OnGet()
+        {
+            try
+            {
+                var customerDtos = await _customerService.GetCustomersAsync(SortColumns, SortOrder, PageIndex, PageSize, GivenName, SurName, City);
+                Customers = _mapper.Map<List<CustomerSearchViewModel>>(customerDtos);
+
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", ex.Message);
+            }
+        }
+    }
+}
