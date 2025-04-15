dbe942a4e533a36b5b14de591ab5b81ce3268a25
Anton Såndberg <saandberg.anton@gmail.com>
2025-04-15 19:39:33 +0200

    feat: add CustomerSearchViewModel

---
+++ BankSystem/ViewModels/CustomerSearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BankSystem.ViewModels
+{
+    public class CustomerSearchViewModel
+    {
+        public int CustomerId { get; set; }
+        public string NationalId { get; set; } = string.Empty;
+        public string Givenname { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Streetaddress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
