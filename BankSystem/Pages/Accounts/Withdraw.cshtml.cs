e6f1f871ec6e16e94226c21d599ee58039adfd37
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-05 14:38:53 +0200

    feat: add Withdraw page

---
+++ BankSystem/Pages/Accounts/Withdraw.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using BankSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.ResultErrors;
+using Services.Services;
+
+namespace BankSystem.Pages.Accounts
+{
+    public class WithdrawModel : PageModel
+    {
+        private readonly IMapper _mapper;
+        private readonly IAccountService _accountService;
+        private readonly ITransactionService _transactionService;
+
+        [BindProperty(SupportsGet = true)]
+        public int AccountId { get; set; }
+
+        [BindProperty]
+        public decimal Amount { get; set; }
+
+        public AccountViewModel? AccountViewModel { get; set; }
+
+        public WithdrawModel(IMapper mapper, IAccountService accountService, ITransactionService transactionService)
+        {
+            _mapper = mapper;
+            _accountService = accountService;
+            _transactionService = transactionService;
+        }
+
+        public async Task OnGetAsync(int accountId)
+        {
+            var dto = await _accountService.GetAccountByIdAsync(accountId);
+            AccountViewModel = _mapper.Map<AccountViewModel>(dto);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var result = await _transactionService.WithdrawAsync(AccountId, Amount);
+
+            if (result.IsFailed)
+            {
+                foreach (var error in result.Errors)
+                {
+                    if (error is FieldError fieldError)
+                    {
+                        ModelState.AddModelError(fieldError.Field, fieldError.Message);
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, error.Message);
+                    }
+                }
+
+                var dto = await _accountService.GetAccountByIdAsync(AccountId);
+                AccountViewModel = _mapper.Map<AccountViewModel>(dto);
+
+                return Page();
+            }
+
+            TempData["SuccessMessage"] = "Withdrawal completed successfully!";
+            return RedirectToPage("/Accounts/AccountDetails", new { accountId = AccountId });
+        }
+    }
+}
