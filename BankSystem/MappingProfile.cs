faa7fdf60d5824a3767fcb7c247dbbd9018927e7
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-05 02:18:28 +0200

    feat: add TransactionViewModel

--- BankSystem/MappingProfile.cs
+++ BankSystem/MappingProfile.cs
@@ -18,6 +18,7 @@ namespace BankSystem
 
             CreateMap<AccountDto, AccountViewModel>().ReverseMap();
 
+            CreateMap<TransactionDto, TransactionViewModel>().ReverseMap();
         }
     }
 }
